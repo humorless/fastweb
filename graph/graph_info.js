1183f648c51e21dfbe657ba8c64c12a77c9a3d11
Laurence Chen <humorless@gmail.com>
2016-11-09 15:41:43 +0800

    enable test option

--- graph/graph_info.js
+++ graph/graph_info.js
@@ -6,13 +6,24 @@ var request = require('request');
 var g = {
   id:'',
   pw:'',
+  e: "cnc-sd-027-209-182-025"
+}
+
+var localURL = {
+  query: "http://10.20.30.40:9966",
+  goAPIHost: "http://10.20.30.40:1234/api/v1"
+}
+
+var URL = {
+  query: "http://query.owl.fastweb.com.cn",
+  goAPIHost: "http://owl.fastweb.com.cn/api/v1"
 }
 
 var graphInfoHandler = function (error, response, body) {
   if (!error && response.statusCode == 200) {
     var json = JSON.parse(body)
-    console.log(json)
-    
+    var filenames = json.map(function(t){ return t.filename })
+    console.log(filenames)
   } else {
     console.log(error)
   }
@@ -22,12 +33,12 @@ var graphInfoHandler = function (error, response, body) {
 var endpointCountersHandler = function (error, response, body) {
   if (!error && response.statusCode == 200) {
     var json = JSON.parse(body)
-    var e = "cnc-sd-027-209-182-025"
+    var e = g.e
     var queryData = json.data.counters.map(function(item){
       return {"endpoint": e, "counter": item}
     })
     var optionGraph = {
-        url: 'http://query.owl.fastweb.com.cn/graph/info',
+        url: URL.query + '/graph/info',
         method: 'POST',
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify(queryData)
@@ -43,10 +54,10 @@ var loginHandler = function (error, response, body) {
     if (!error && response.statusCode == 200) {
         // Print out the response body
         var json = JSON.parse(body)
-        var e = '["cnc-sd-027-209-182-025"]'
+        var e = JSON.stringify([g.e])
         var m = '.+'
         var options = {
-            url: 'http://owl.fastweb.com.cn/api/v1/dashboard/endpointcounters',
+            url: URL.goAPIHost + '/dashboard/endpointcounters',
             method: 'POST',
             headers: {"Content-Type": "application/x-www-form-urlencoded"},
             form: {'cName': g.id, 'cSig': json.data.sig, 'endpoints': e, 'metricQuery':m}
@@ -59,7 +70,7 @@ var loginHandler = function (error, response, body) {
 
 // Configure the request
 var options = {
-    url: 'http://owl.fastweb.com.cn/api/v1/auth/login',
+    url: URL.goAPIHost + '/auth/login',
     method: 'POST',
     headers: {"Content-Type": "application/x-www-form-urlencoded"},
     form: {'name': g.id, 'password': g.pw}
